480b064e89a857590d9615a2b7b8e1650379b556
waortiz <waortiz@hotmail.com>
2024-01-25 07:37:13 -0500

    Se agregan clases

---
+++ SaludDental/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace SaludDental.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set;}
+        public string PrimerApellido { get; set;}
+        public string SegundoApellido { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
+        public string NumeroDocumento { get; set; }
+    }
+}
---
+++ SaludDental/TipoDocumento.cs
@@ -0,0 +1,8 @@
+﻿namespace SaludDental.Entidades
+{
+    public class TipoDocumento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
